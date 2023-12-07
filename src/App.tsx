ec752b860a5443fcc112c458a7b038de171c8c20
kevinduhamelhayes <kevinduhamelh@gmail.com>
2023-12-07 15:37:08 -0300

    hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import { ListOfUsers } from "./components/ListOfUsers";
+import { CreateNewUser } from './components/CreateNewUser';
+import { Toaster } from 'sonner'
 
 function App() {
 	return (
 		<>
-			<div>
-				<h1>Proyecto redux</h1>
-			</div>
+			<ListOfUsers />
+			<CreateNewUser />
+			<Toaster richColors />
 		</>
 	);
 }
